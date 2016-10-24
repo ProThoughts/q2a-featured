972009ea92640c6d6d3f571c68b360e8f0082cb0
ProThoughts <prothoughts.com@gmail.com>
2016-10-23 22:41:03 -0700

    Update qa-featured-question-admin.php

--- qa-featured-question-admin.php
+++ qa-featured-question-admin.php
@@ -1,10 +1,10 @@
 <?php
-    class qa_featured_admin {
+    class pt_qa_featured_question_admin {
 
 	function option_default($option) {
 		
 	    switch($option) {
-		case 'featured_question_css':
+		case 'pt_q2a_featured_question_css':
 		    return '
 .qa-q-list-item-featured {
     background-color:#FFC;
@@ -28,9 +28,9 @@
             
             $ok = null;
             
-            if (qa_clicked('featured_questions_save')) {
-                qa_opt('featured_questions_list',qa_post_text('featured_questions_list'));
-                qa_opt('featured_question_css',qa_post_text('featured_question_css'));
+            if (qa_clicked('pt_q2a_featured_questions_save')) {
+                qa_opt('pt_q2a_featured_questions_list',qa_post_text('pt_q2a_featured_questions_list'));
+                qa_opt('pt_q2a_featured_question_css',qa_post_text('pt_q2a_featured_question_css'));
 		
 		$ok = qa_lang('admin/options_saved');
             }
@@ -41,15 +41,15 @@
     
 	    $fields[] = array(
 		'label' => 'Featured question ids',
-		'tags' => 'NAME="featured_questions_list"',
-		'value' => qa_opt('featured_questions_list'),
+		'tags' => 'NAME="pt_q2a_featured_questions_list"',
+		'value' => qa_opt('pt_q2a_featured_questions_list'),
 		'type' => 'text',
 		'note' => 'separate question id numbers with commas (e.g. 123,456)',
 	    );
 	    $fields[] = array(
                 'label' => 'Custom css:',
-                'tags' => 'NAME="featured_question_css"',
-                'value' => qa_opt('featured_question_css'),
+                'tags' => 'NAME="pt_q2a_featured_question_css"',
+                'value' => qa_opt('pt_q2a_featured_question_css'),
 		'rows' => 10,
                 'type' => 'textarea',
             );
@@ -61,7 +61,7 @@
                 'buttons' => array(
                     array(
                         'label' => qa_lang_html('main/save_button'),
-                        'tags' => 'NAME="featured_questions_save"',
+                        'tags' => 'NAME="pt_q2a_featured_questions_save"',
                     ),
                 ),
             );
